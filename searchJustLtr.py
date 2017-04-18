5aa4ac976c896469646ab0f70d663edb77129520
Doug Turnbull <softwaredoug@gmail.com>
2017-04-17 21:39:17 -0400

    Save the LTR query template

--- searchJustLtr.py
+++ searchJustLtr.py
@@ -1,4 +1,5 @@
 from features import formatFeature
+import json
 
 baseQuery = {
   "query": {
@@ -11,16 +12,16 @@ baseQuery = {
   }
 }
 
-def featureQueries(keywords):
+def featureQueries(keywords, model):
     try:
         ftrId = 1
         while True:
-            parsedJson = formatFeature(ftrId, keywords)
+            parsedJson = formatFeature(ftrId, "{{query_string}}")
             baseQuery['query']['ltr']['features'].append(parsedJson['query'])
             ftrId += 1
     except IOError:
         pass
-    import json
+    baseQuery['query']['ltr']['model']['stored'] = model
     print("%s" % json.dumps(baseQuery))
     return baseQuery
 
@@ -29,12 +30,12 @@ if __name__ == "__main__":
     from sys import argv
     from elasticsearch import Elasticsearch
     esUrl="http://localhost:9200"
-    es = Elasticsearch(timeout=1000)
-    search = featureQueries(argv[1])
     model = "test_6"
     if len(argv) > 2:
         model = argv[2]
-    baseQuery['query']['ltr']['model']['stored'] = model
+    es = Elasticsearch(timeout=1000)
+    search = featureQueries(argv[1], model)
+    es.put_template('o19s_blog_search', body=json.dumps(baseQuery['query']))
     results = es.search(index='o19s', doc_type='post', body=search)
     for result in results['hits']['hits']:
              print("%s, %s" % (result['_id'], result['_source']['title']))
